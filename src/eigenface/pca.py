7a16978ea3d6c34b613907034c47e03c27ef82c9
Roope <roope.kaikkonen@helsinki.fi>
2026-04-11 19:43:22 +0300

    fix logic in pca.py

--- src/eigenface/pca.py
+++ src/eigenface/pca.py
@@ -8,12 +8,11 @@ def compute_mean(A: Matrix) -> Vector:
     cols = len(A[0])
 
     mean: Vector = []
-    for r in range(rows):
-        summa = 0.0
-        for c in range(cols):
-            summa += A[r][c]
-        mean.append(summa / cols)
-
+    for c in range(cols):
+        summa=0.0
+        for r in range(rows):
+            summa+=A[r][c]
+        mean.append(summa/rows)
     return mean
 
 def center_data(Data: Matrix, Mean: Vector) -> Matrix:
@@ -26,7 +25,7 @@ def center_data(Data: Matrix, Mean: Vector) -> Matrix:
     for i in range(rows):
         centered.append([])
         for j in range(cols):
-            centered_val = Data[i][j] - Mean[i]
+            centered_val = Data[i][j] - Mean[j]
             centered[i].append(centered_val)
 
     return centered
@@ -35,7 +34,7 @@ def center_data(Data: Matrix, Mean: Vector) -> Matrix:
 def covariance(A:Matrix)->Matrix:
     AT=Transpose(A)
     C=MatrixMultiplication(AT,A)
-    M=len(A[0])
+    M=len(A)
     for i in range(len(C)):
         for j in range(len(C[0])):
             C[i][j] = C[i][j]/ M
