0ea100a07bbf0bb3f708b4ab919475e57f225d9c
Roope <roope.kaikkonen@helsinki.fi>
2026-04-24 19:40:57 +0300

    Added splitting of data totraining and holdout data for testing

--- src/eigenface/app.py
+++ src/eigenface/app.py
@@ -10,24 +10,34 @@ if __name__ == "__main__":
     k=20
     iterations=10
     tolerance=1e-4
-
+    threshold=9
+    distances=[]
     start = time.time()
 
     print("Loading training images")
-    test_matrix=load_dataset(folder_path,heigth,width)
-    training_matrix=test_matrix[:-1]
-    test_face=test_matrix[-1]
-    print(f"Succesfully loaded {len(training_matrix)} amount of images")
+    test_matrix,train_matrix=load_dataset(folder_path,heigth,width)
+    training_matrix=train_matrix
+    print(f"Succesfully loaded {len(training_matrix)} amount of training images and {len(test_matrix)} of testing images")
 
     print("Training eigenfaces")
     mean, eigenvalues,eigenfaces,train_weights=train_eigenfaces(training_matrix,k,iterations,tolerance)
     print(f"Succesfully computed {len(eigenfaces)} top eigenfaces")
 
 
-    print("Running test on test_image")
-    test_weigths=predict_face(test_face,mean,eigenfaces)
-    best_index,nearest_distance=nearest_neighbor(test_weigths,train_weights)
+    print("Running tests on test_matrix")
+    for test_face in test_matrix:
+        test_weigths=predict_face(test_face,mean,eigenfaces)
+        best_index,nearest_distance=nearest_neighbor(test_weigths,train_weights)
+        print(f"Got result: index={best_index} and distance={nearest_distance}")
+        distances.append(nearest_distance)
+        if nearest_distance<= threshold:
+            print("known face")
+        else:
+            print("not recognized")
+
     end = time.time()
-    print(f"Got result: index={best_index} and distance={nearest_distance}")
 
     print(f"Total runtime of the program is {end - start} seconds")
+    print(distances)
+    avg=sum(distances)/len(distances)
+    print((f"Average={avg}, min={min(distances)},max={max(distances)}"))
--- src/eigenface/dataset.py
+++ src/eigenface/dataset.py
@@ -11,13 +11,17 @@ def load_dataset(folder_path:str, height:int, width:int):
         filepath= join(folder_path,folder)
         for file in listdir(filepath):
             image_path=join(filepath,file)
-            if isfile(image_path) and len(matrix)< 251:
+            if isfile(image_path) and len(matrix)< 2510:
                 vector=load_image_as_vector(image_path,height,width)
                 matrix.append(vector)
     if not matrix:
         raise ValueError("no images found in folder")
-
-    return matrix
+    n=len(matrix)
+    holdout_size= max(1,round(0.2*n))
+    split=n-holdout_size
+    test_matrix= matrix[:split]
+    training_matrix=matrix[split:]
+    return training_matrix, test_matrix
 
 
 def load_image_as_vector(image_path:str,height:int,width:int):
